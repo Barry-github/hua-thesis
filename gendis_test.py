d6e7918cf892b2bac6df6b0747dd95a14d917d87
gustrip <itp17110@hua.gr>
2018-12-28 00:13:46 +0200

    test commit

--- gendis_test.py
+++ gendis_test.py
@@ -2,25 +2,24 @@ import numpy as np
 from gendis.genetic import GeneticExtractor
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-
-np.random.seed(1337)  # Random seed for reproducibility
-
 from data_extraction import DataExtractor
 from trajectory_generator import TrajectoryGenerator
 
+np.random.seed(1337)  # Random seed for reproducibility
 
-#create files if not created
+# Create files if not created
+
 tr_gen = TrajectoryGenerator(samples=100)
 tr_gen.data_generation(n_test=10)
 
 # Read in the datafiles
 dex = DataExtractor()
-train_df,test_df = dex.train_test_dataframes()
+train_df, test_df = dex.train_test_dataframes()
 dex.define_csv(dataset=train_df, ts_class="Bearing", file="train.csv")
 dex.define_csv(dataset=test_df, ts_class="Bearing", file="test.csv")
 
 x_train, y_train, x_test, y_test = dex.load_datasets()
-genetic_extractor =  GeneticExtractor(population_size=20, iterations=25, verbose=True,
+genetic_extractor = GeneticExtractor(population_size=20, iterations=25, verbose=True,
                                      normed=False, add_noise_prob=0.3, add_shapelet_prob=0.3,
                                      wait=10, plot=None, remove_shapelet_prob=0.3,
                                      crossover_prob=0.66, n_jobs=4)
