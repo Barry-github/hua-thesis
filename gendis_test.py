80cb68b1e44a65e1b083cce99a0dffca2b4f1a47
gustrip <itp17110@hua.gr>
2019-01-15 22:06:17 +0200

    creations of scenarios for automated experiments

--- gendis_test.py
+++ gendis_test.py
@@ -2,17 +2,18 @@ import numpy as np
 from gendis.genetic import GeneticExtractor
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from gendis_test.data_extraction import DataExtractor
-from gendis_test.trajectory_generator import TrajectoryGenerator
-from gendis_test.utils import standardize_data
-
+from tools.data_extraction import DataExtractor
+from tools.trajectory_generator import TrajectoryGenerator
+from tools.utils import standardize_data
+#from tools.experiments import Experiments
 np.random.seed(1337)  # Random seed for reproducibility
 
 # Create files if not created
-reset_data = False
-#reset_data = True
-tr_gen = TrajectoryGenerator(samples=100, reset_data=reset_data)
-tr_gen.data_generation(n_test=10)
+#exp = Experiments
+#scenarios = experiment_scenarios()
+
+tr_gen = TrajectoryGenerator(samples=100, reset_data=True)
+tr_gen.data_generation(n_test=50)
 
 # Read in the datafiles
 dex = DataExtractor()
@@ -25,10 +26,20 @@ x_train, y_train, x_test, y_test = dex.load_datasets()
 print("standardized train and test data\n")
 x_train, x_test = standardize_data(x_train, x_test)
 
-genetic_extractor = GeneticExtractor(population_size=25, iterations=25, verbose=True,
-                                     normed=True, add_noise_prob=0.3, add_shapelet_prob=0.3,
-                                     wait=10, plot=None, remove_shapelet_prob=0.3,
-                                     crossover_prob=0.66, n_jobs=4)
+genetic_options = {"population_size": 25,
+                   "iterations": 25,
+                   "verbose": True,
+                   "normed": True,
+                   "add_noise_prob": 0.1,
+                   "add_shapelet_prob": 0.1,
+                   "wait": 10,
+                   "plot": None,
+                   "remove_shapelet_prob": 0.1,
+                   "crossover_prob": 0.8,
+                   "n_jobs": 4
+                   }
+
+genetic_extractor = GeneticExtractor(**genetic_options)
 print("Starting fit in genetic extractor with:\n"
       "population size:{0:d}\n"
       "iterations: {1:d}\n"
